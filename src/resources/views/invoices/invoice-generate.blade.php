0b30e604a6b92a5865a78aeaa10a77cc5da6487d
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-06-19 05:59:28 +0300

    arabic chars pdf

--- src/resources/views/invoices/invoice-generate.blade.php
+++ src/resources/views/invoices/invoice-generate.blade.php
@@ -331,7 +331,7 @@
     </style>
 </head>
 
-<body style="font-family: arial;">
+<body style="font-family: Arial;">
 <div class="invoice_container">
     <div class="invoice_container__item px-5 mt-5 text-black">
         <div class="w-25 f-left p-2">
