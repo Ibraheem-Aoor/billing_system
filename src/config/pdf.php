9bde0594242546d44b5ee55e76ec68dcb80a60f8
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-06-19 09:51:36 +0300

    pdffff

--- src/config/pdf.php
+++ src/config/pdf.php
@@ -16,14 +16,15 @@ return [
   'useKashida' => 75,  
   'font_path' => __DIR__.'/../resources/fonts/',
 	'font_data' => [
-		'arial' => [
-			'R'  => '../public/assets/arial.ttf',    // regular font
-			'B'  => '../public/assets/arial.ttf',       // optional: bold font
-			'I'  => '../public/assets/arial.ttf',     // optional: italic font
-			'BI' => '../public/assets/arial.ttf', // optional: bold-italic font
-			'useOTL' => 0xFF,    // required for complicated langs like Persian, Arabic and Chinese
-			'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
-      ],
+    'dejavu sans' =>
+            [
+                'R' => $distFontDir . '/DejaVuSans-Bold',
+                'B' => $distFontDir . '/DejaVuSans-BoldOblique',
+                'I' => $distFontDir . '/DejaVuSans-Oblique',
+                'BI' => $distFontDir . '/DejaVuSans',
+                'useOTL' => 0xFF,    // required for complicated langs like Persian, Arabic and Chinese
+                'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
+            ],
 		'useOTL' => 0xFF,    // required for complicated langs like Persian, Arabic and Chinese
 		'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
 		// ...add as many as you want.
