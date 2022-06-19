fc14b2d3534a53cae7eae0d585f1f962a469fd9b
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-06-19 08:53:08 +0300

    pdf

--- src/resources/views/layouts/includes/sidebar.blade.php
+++ src/resources/views/layouts/includes/sidebar.blade.php
@@ -1,6 +1,6 @@
 <app-sidebar :data="{{ json_encode($data) }}" :logo="{{ json_encode(config('settings.application.company_logo')) }}"
     :logo-icon="{{ json_encode(config('settings.application.company_icon')) }}">
+    <script>
+        $(".horizontal-menu .nav li:last").hide();
+    </script>
 </app-sidebar>
-<script>
-    $(".horizontal-menu .nav li:last").hide();
-</script>
