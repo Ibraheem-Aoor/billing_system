bd096401773aacf8db526e0025901e8bffe4f801
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-06-19 05:49:07 +0300

    arabic chars pdf

--- src/resources/views/invoices/invoice-generate.blade.php
+++ src/resources/views/invoices/invoice-generate.blade.php
@@ -1,7 +1,7 @@
 <!Doctype html>
 <html>
 <head>
-    <meta charset="UTF-8"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
@@ -33,7 +33,7 @@
         }
 
         * {
-            font-family: DejaVu Sans, sans-serif; 
+            font-family: Arial, Helvetica, sans-serif;
             margin: 0;
             padding: 0;
         }
