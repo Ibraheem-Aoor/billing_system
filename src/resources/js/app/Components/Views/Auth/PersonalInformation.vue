c029d593f384fd48122b708e7f6398f0cdc93bdc
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-07-01 21:07:41 +0300

    fixing

--- src/resources/js/app/Components/Views/Auth/PersonalInformation.vue
+++ src/resources/js/app/Components/Views/Auth/PersonalInformation.vue
@@ -205,7 +205,7 @@ export default {
       this.preloader = true;
     },
     submit() {
-      let submitData = this.user.loggedInUser;
+      let submitData = {...this.user.loggedInUser , ...this.user.trn};
       if (submitData.date_of_birth)
         submitData.date_of_birth = this.getDateFormatForBackend(
           submitData.date_of_birth
